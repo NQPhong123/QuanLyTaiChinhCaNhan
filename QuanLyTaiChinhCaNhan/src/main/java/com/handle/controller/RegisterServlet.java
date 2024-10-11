33c7b0716495e61c9de2f89e290d397102df7009
NQPhong123 <2251120232@ut.edu.vn>
2024-10-11 19:58:26 +0700

    .

--- QuanLyTaiChinhCaNhan/src/main/java/com/handle/controller/RegisterServlet.java
+++ QuanLyTaiChinhCaNhan/src/main/java/com/handle/controller/RegisterServlet.java
@@ -6,65 +6,31 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Pattern;
-import jakarta.servlet.http.HttpSession;
+
 import com.handle.dao.UserDao;
 import com.handle.model.User;
 
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String confirmPassword = request.getParameter("confirm_password");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String email = request.getParameter("email"); 
+		String password = request.getParameter("password");
+		
+		User user = new User();
+		user.setEmail(email);
+		user.setPassWord(password);
+		
+		UserDao userDao = new UserDao();
+		if(userDao.saveUser(user)) {
+			response.sendRedirect("login");
+		}else {
+			request.setAttribute("errorMessage", "Registration failed. Please try again.");
+			request.getRequestDispatcher("register").forward(request, response);
+		}
+	}
 
-        // Regex for email and password
-        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
-        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$"; // At least 8 chars, 1 digit, 1 lower, 1 upper case
-
-        boolean hasError = false;
-
-        // Validate email
-        if (!Pattern.matches(emailRegex, email)) {
-            request.setAttribute("emailError", "Nhập sai email");
-            hasError = true;
-        }
-
-        // Validate password
-        if (!Pattern.matches(passwordRegex, password)) {
-            request.setAttribute("passwordError", "Phải có 8 kí tự, có chữ hoa, thường và số");
-            hasError = true;
-        }
-
-        // Confirm password match
-        if (!password.equals(confirmPassword)) {
-            request.setAttribute("confirmPasswordError", "Mật khẩu không khớp");
-            hasError = true;
-        }
-
-        // If there is an error, redirect back to register page
-        if (hasError) {
-            request.getRequestDispatcher("register").forward(request, response);
-            return;
-        }
-
-        // Save user if no errors
-        User user = new User();
-        user.setEmail(email);
-        user.setPassWord(password);
-
-        UserDao userDao = new UserDao();
-        if (userDao.saveUser(user)) {
-        	 HttpSession session = request.getSession();
-             session.setAttribute("successMessage", "Đăng kí thành công! Bạn có thể đăng nhập.");
-            response.sendRedirect("login");
-        } else {
-            request.setAttribute("errorMessage", "Email đã tồn tại, vui lòng thử lại");
-            request.getRequestDispatcher("register").forward(request, response);
-        }
-    }
 }
--- QuanLyTaiChinhCaNhan/src/main/java/com/handle/dao/UserDao.java
+++ QuanLyTaiChinhCaNhan/src/main/java/com/handle/dao/UserDao.java
@@ -5,68 +5,52 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import helper.BCrypt;
 
 public class UserDao {
 
-    // Lưu người dùng
-    public boolean saveUser(User user) {
-        boolean status = false;
-        Connection conn = ConnectDB.getConection();
-        if (conn != null) {
-            try {
-                String query = "INSERT INTO User(Email,PasswordHash) VALUES(?,?)";
-                PreparedStatement ptst = conn.prepareStatement(query);
-                ptst.setString(1, user.getEmail());
-                ptst.setString(2, user.getPassWord());
-                status = ptst.executeUpdate() > 0;
-                ConnectDB.closeConnection(conn);
-            } catch (SQLException e) {
-                e.getStackTrace();
-                System.out.println(e.getMessage());
-            }
-        }
-        return status;
-    }
+	// Lưu người dùng
+	public boolean saveUser(User user) {
+		boolean status = false;
+		Connection conn = ConnectDB.getConection();
+		String encodePass = BCrypt.hashpw(user.getPassWord(), BCrypt.gensalt());
+		if (conn != null) {
+			try {
+				String query = "INSERT INTO User(Email,PasswordHash) VALUES(?,?)";
+				PreparedStatement ptst = conn.prepareStatement(query);
+				ptst.setString(1, user.getEmail());
+				ptst.setString(2, encodePass);
+				status = ptst.executeUpdate() > 0;
+				ConnectDB.closeConnection(conn);
+			} catch (SQLException e) {
+				e.getStackTrace();
+				System.out.println(e.getMessage());
+			}
+		}
+		return status;
+	}
 
-    // Kiểm tra email và mật khẩu có khớp không
-    public boolean checkUser(User user) {
-        Connection conn = ConnectDB.getConection();
-        boolean status = false;
-        if (conn != null) {
-            try {
-                String query = "SELECT * FROM User WHERE Email = ? AND PasswordHash = ?";
-                PreparedStatement ptst = conn.prepareStatement(query);
-                ptst.setString(1, user.getEmail());
-                ptst.setString(2, user.getPassWord());
-                ResultSet resultSet = ptst.executeQuery();
-                // Nếu tìm thấy bản ghi, tức là email và mật khẩu đúng
-                status = resultSet.next();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return status;
-    }
+	// Kiểm tra người dùng có tồn tại
+	public boolean checkUser(User user) {
+		Connection conn = ConnectDB.getConection();
+		boolean status = false;
+		if (conn != null) {
+			try {
+				String query = "SELECT PasswordHash FROM User WHERE Email = ?";
+				PreparedStatement ptst = conn.prepareStatement(query);
+				ptst.setString(1, user.getEmail());
+				ResultSet resultSet = ptst.executeQuery();
+				while (resultSet.next()) {
+					String passwordHash = resultSet.getString("PasswordHash");
+					status = BCrypt.checkpw(user.getPassWord(), passwordHash);
+				}
 
-    // Lấy thông tin người dùng dựa trên email
-    public User getUserByEmail(String email) {
-        User user = null;
-        try (Connection connection = ConnectDB.getConection()) {
-            String query = "SELECT * FROM User WHERE email = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, email);
-            ResultSet resultSet = statement.executeQuery();
+			} catch (SQLException e) {
+				e.getStackTrace();
+				System.out.println(e.getMessage());
+			}
+		}
+		return status;
+	}
 
-            if (resultSet.next()) {
-                user = new User();
-                user.setEmail(resultSet.getString("email"));
-                user.setPassWord(resultSet.getString("password"));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return user;
-    }
 }
