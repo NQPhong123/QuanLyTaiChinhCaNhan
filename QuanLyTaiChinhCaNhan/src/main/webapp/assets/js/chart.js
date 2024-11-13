daf143a7098e4d4c0e49872f02e6fa51d55131d1
your name <2251120246@ut.edu.vn>
2024-11-13 21:53:48 +0700

    ChartSeverlet, report,chart,income

--- QuanLyTaiChinhCaNhan/src/main/webapp/assets/js/chart.js
+++ QuanLyTaiChinhCaNhan/src/main/webapp/assets/js/chart.js
@@ -1,25 +1,25 @@
 const revenueCtx = document.getElementById('revenueChart').getContext('2d');
 const revenueChart = new Chart(revenueCtx, {
-	type: 'doughnut',
-	data: {
-		labels: ['Lương', 'Chứng khoán'],
-		datasets: [{
-			data: [6000000, 1200000], // Dữ liệu sẽ được cập nhật động
-			backgroundColor: ['#36a2eb', '#ff6384'],
-			hoverOffset: 4
-		}]
-	}
+    type: 'doughnut',
+    data: {
+        labels: ['Lương', 'Chứng khoán'],
+        datasets: [{
+            data: [0, 0], // Dữ liệu sẽ được cập nhật động từ server
+            backgroundColor: ['#36a2eb', '#ff6384'],
+            hoverOffset: 4
+        }]
+    }
 });
 
 const expenseCtx = document.getElementById('expenseChart').getContext('2d');
 const expenseChart = new Chart(expenseCtx, {
-	type: 'doughnut',
-	data: {
-		labels: ['Tiền chơi xe', 'Tiền cho gái'],
-		datasets: [{
-			data: [100000, 200000], // Dữ liệu sẽ được cập nhật động
-			backgroundColor: ['#36a2eb', '#ff6384'],
-			hoverOffset: 4
-		}]
-	}
+    type: 'doughnut',
+    data: {
+        labels: ['Mua sắm', 'Dịch vụ'],
+        datasets: [{
+            data: [0, 0], // Dữ liệu sẽ được cập nhật động từ server
+            backgroundColor: ['#ff9f40', '#ffcd56'],
+            hoverOffset: 4
+        }]
+    }
 });
