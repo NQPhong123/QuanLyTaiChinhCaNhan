89d86ef2dca72edc1ebf07d3cc3482c9b6c978e9
Tai0200 <2251120243@ut.edu.vn>
2024-11-22 03:59:44 +0700

    Update CategoryDAO.java

--- QuanLyTaiChinhCaNhan/src/main/java/com/handle/dao/CategoryDAO.java
+++ QuanLyTaiChinhCaNhan/src/main/java/com/handle/dao/CategoryDAO.java
@@ -33,5 +33,53 @@ public class CategoryDAO {
 		}
 		return categories;
 	}
+	// Phương thức lấy tên category từ categoryID
+    public String getCategoryNameByID(int categoryID) {
+        String categoryName = null;
+        String query = "SELECT CategoryName FROM category WHERE CategoryID = ?";
+        
+        try {
+            Connection conn = ConnectDB.getInstance().getConnection();
+            PreparedStatement ptst = conn.prepareStatement(query);
+            ptst.setInt(1, categoryID);
+            ResultSet rs = ptst.executeQuery();
+            
+            if (rs.next()) {
+                categoryName = rs.getString("CategoryName");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error fetching category name: " + e.getMessage());
+        }
+        
+        return categoryName;
+    }
+    
+    
+ // Phương thức lấy URL_Image từ categoryID
+    public String getCategoryImageURLByID(int categoryID) {
+        String URL_Image  = null;
+        String query = "SELECT URL_Image FROM category WHERE CategoryID = ?";
+        
+        try {
+            // Kết nối đến cơ sở dữ liệu
+            Connection conn = ConnectDB.getInstance().getConnection();
+            
+            // Chuẩn bị câu lệnh SQL với tham số categoryID
+            PreparedStatement ptst = conn.prepareStatement(query);
+            ptst.setInt(1, categoryID);
+            
+            // Thực thi câu lệnh truy vấn
+            ResultSet rs = ptst.executeQuery();
+            
+            // Kiểm tra nếu có dữ liệu trả về và lấy URL_Image
+            if (rs.next()) {
+            	URL_Image = rs.getString("URL_Image");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error fetching category image URL: " + e.getMessage());
+        }
+        
+        return URL_Image;
+    }
 
 }
